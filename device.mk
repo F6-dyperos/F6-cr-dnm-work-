8a5798d310fab884266b78bda453388936e7f61d
Arian <arian.kulmer@web.de>
2024-10-06 12:25:37 +0200

    peridot: Import qcril_database and generate the db at build time
    Imported from diting V816.0.7.0.ULFMIXM
    
    Change-Id: I7798bd3c33d3b6f801e8f48a06fe994947715db9
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -586,6 +586,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     framework-permission-aidl-cpp.vendor
 
+PRODUCT_PACKAGES += \
+    qcrilNrDb_vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
