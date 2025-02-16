crdroidandroid/android_device_xiaomi_peridot

peridot: parts: Add per app maximum refresh rate
* Adapt to peridot
* Drop 30/50/90hz  (We do not support these values)

peridot: parts: Simplify per-app refresh rate strings

peridot: parts: Improve per-app refresh rate activity

peridot: parts: Refresh rate's app list improvement

peridot: parts: Thermal Profiles and Per-app refresh rate improvements

peridot: parts: Set default refresh rate to 120Hz

peridot: parts: Set correct default icon to per-app refresh rate

peridot: parts: Fix compile after QPR2 merge

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
+import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -29,6 +30,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             return;
         }
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
+
+        // Refreshrate
+        RefreshUtils.startService(context);
 
         // Thermal
         ThermalUtils.startService(context);

parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.refreshrate;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+public class RefreshActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_REFRESH = "refresh";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new RefreshSettingsFragment(), TAG_REFRESH).commit();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}

parts/src/org/lineageos/settings/refreshrate/RefreshService.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.refreshrate;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.ActivityTaskManager.RootTaskInfo;
+import android.app.IActivityTaskManager;
+import android.app.TaskStackListener;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.os.RemoteException;
+
+public class RefreshService extends Service {
+
+    private static final String TAG = "RefreshService";
+    private static final boolean DEBUG = false;
+
+    private String mPreviousApp;
+    private RefreshUtils mRefreshUtils;
+    private IActivityTaskManager mActivityTaskManager;
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mPreviousApp = "";
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        try {
+            mActivityTaskManager = ActivityTaskManager.getService();
+            mActivityTaskManager.registerTaskStackListener(mTaskListener);
+        } catch (RemoteException e) {
+            // Do nothing
+        }
+        mRefreshUtils = new RefreshUtils(this);
+        registerReceiver();
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);        
+        this.registerReceiver(mIntentReceiver, filter);
+    }
+
+     private final TaskStackListener mTaskListener = new TaskStackListener() {
+        @Override
+        public void onTaskStackChanged() {
+            try {
+                final RootTaskInfo info = mActivityTaskManager.getFocusedRootTaskInfo();
+                if (info == null || info.topActivity == null) {
+                    return;
+                }
+                String foregroundApp = info.topActivity.getPackageName();
+                if (!mRefreshUtils.isAppInList) {
+                 mRefreshUtils.getOldRate();
+                  } 
+                if (!foregroundApp.equals(mPreviousApp)) {
+                    mRefreshUtils.setRefreshRate(foregroundApp);
+                    mPreviousApp = foregroundApp;
+                  }
+ 		 } catch (Exception e) {}
+            }
+        };
+    }

parts/src/org/lineageos/settings/refreshrate/RefreshUtils.java
@@ -1,0 +1,141 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.refreshrate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.view.Display;
+
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+public final class RefreshUtils {
+
+    private static final String REFRESH_CONTROL = "refresh_control";
+
+    private static float defaultMaxRate;
+    private static float defaultMinRate;
+    private static final String KEY_PEAK_REFRESH_RATE = "peak_refresh_rate";
+    private static final String KEY_MIN_REFRESH_RATE = "min_refresh_rate";
+    private Context mContext;
+    protected static boolean isAppInList = false;
+
+    protected static final int STATE_DEFAULT = 0;
+    protected static final int STATE_STANDARD = 1;
+    protected static final int STATE_EXTREME = 2;
+
+    private static final float REFRESH_STATE_DEFAULT = 120f;
+    private static final float REFRESH_STATE_STANDARD = 60f;
+    private static final float REFRESH_STATE_EXTREME = 120f;
+
+    private static final String REFRESH_STANDARD = "refresh.standard=";
+    private static final String REFRESH_EXTREME = "refresh.extreme=";
+
+    private SharedPreferences mSharedPrefs;
+
+    protected RefreshUtils(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        mContext = context;
+    }
+
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, RefreshService.class),
+                UserHandle.CURRENT);
+    }
+
+    private void writeValue(String profiles) {
+        mSharedPrefs.edit().putString(REFRESH_CONTROL, profiles).apply();
+    }
+
+   protected void getOldRate(){
+        defaultMaxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, REFRESH_STATE_DEFAULT);
+        defaultMinRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, REFRESH_STATE_DEFAULT);
+    }
+
+
+    private String getValue() {
+        String value = mSharedPrefs.getString(REFRESH_CONTROL, null);
+
+        if (value == null || value.isEmpty()) {
+            value = REFRESH_STANDARD + ":" + REFRESH_EXTREME;
+            writeValue(value);
+        }
+        return value;
+    }
+
+    protected void writePackage(String packageName, int mode) {
+        String value = getValue();
+        value = value.replace(packageName + ",", "");
+        String[] modes = value.split(":");
+        String finalString;
+
+        switch (mode) {
+            case STATE_STANDARD:
+                modes[0] = modes[0] + packageName + ",";
+                break;
+            case STATE_EXTREME:
+                modes[1] = modes[1] + packageName + ",";
+                break;
+        }
+
+        finalString = modes[0] + ":" + modes[1];
+
+        writeValue(finalString);
+    }
+
+    protected int getStateForPackage(String packageName) {
+        String value = getValue();
+        String[] modes = value.split(":");
+        int state = STATE_DEFAULT;
+        if (modes[0].contains(packageName + ",")) {
+            state = STATE_STANDARD;
+        } else if (modes[1].contains(packageName + ",")) {
+            state = STATE_EXTREME;
+        }
+        return state;
+    }
+
+    protected void setRefreshRate(String packageName) {
+        String value = getValue();
+        String modes[];
+        float maxrate = defaultMaxRate;
+        float minrate = defaultMinRate;
+        isAppInList = false;
+
+            if (value != null) {
+            modes = value.split(":");
+
+            if (modes[0].contains(packageName + ",")) {
+                maxrate = REFRESH_STATE_STANDARD;
+                if ( minrate > maxrate){
+                minrate = maxrate;
+                }
+		isAppInList = true;
+           } else if (modes[1].contains(packageName + ",")) {
+                maxrate = REFRESH_STATE_EXTREME;
+                if ( minrate > maxrate){
+                minrate = maxrate;
+                }
+		isAppInList = true;
+           }
+          }
+	Settings.System.putFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, minrate);
+        Settings.System.putFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, maxrate);
+    }
+}

F6-dyperos/F6-cr-dnm-work-

Signed-off-by: ramshell688 <ramonzost68@gmail.com>

parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -17,6 +17,7 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
+import com.xiaomi.settings.refreshrate.RefreshUtils;
 import com.xiaomi.settings.touchsampling.TouchSamplingUtils;
 import com.xiaomi.settings.touchsampling.TouchSamplingService;
 import com.xiaomi.settings.touchsampling.TouchSamplingTileService;
@@ -41,6 +42,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         TouchSamplingUtils.restoreSamplingValue(context);
         context.startServiceAsUser(new Intent(context, TouchSamplingService.class),
                 UserHandle.CURRENT);
+
+        // Refreshrate
+        RefreshUtils.startService(context);
 
         // Touch Sampling Tile Service
         context.startServiceAsUser(new Intent(context, TouchSamplingTileService.class),

parts/src/com/xiaomi/settings/refreshrate/RefreshActivity.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.refreshrate;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+public class RefreshActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_REFRESH = "refresh";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new RefreshSettingsFragment(), TAG_REFRESH).commit();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}

parts/src/com/xiaomi/settings/refreshrate/RefreshService.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.refreshrate;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.ActivityTaskManager.RootTaskInfo;
+import android.app.IActivityTaskManager;
+import android.app.TaskStackListener;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.os.RemoteException;
+
+public class RefreshService extends Service {
+
+    private static final String TAG = "RefreshService";
+    private static final boolean DEBUG = false;
+
+    private String mPreviousApp;
+    private RefreshUtils mRefreshUtils;
+    private IActivityTaskManager mActivityTaskManager;
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mPreviousApp = "";
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        try {
+            mActivityTaskManager = ActivityTaskManager.getService();
+            mActivityTaskManager.registerTaskStackListener(mTaskListener);
+        } catch (RemoteException e) {
+            // Do nothing
+        }
+        mRefreshUtils = new RefreshUtils(this);
+        registerReceiver();
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);        
+        this.registerReceiver(mIntentReceiver, filter);
+    }
+
+     private final TaskStackListener mTaskListener = new TaskStackListener() {
+        @Override
+        public void onTaskStackChanged() {
+            try {
+                final RootTaskInfo info = mActivityTaskManager.getFocusedRootTaskInfo();
+                if (info == null || info.topActivity == null) {
+                    return;
+                }
+                String foregroundApp = info.topActivity.getPackageName();
+                if (!mRefreshUtils.isAppInList) {
+                 mRefreshUtils.getOldRate();
+                  } 
+                if (!foregroundApp.equals(mPreviousApp)) {
+                    mRefreshUtils.setRefreshRate(foregroundApp);
+                    mPreviousApp = foregroundApp;
+                  }
+ 		 } catch (Exception e) {}
+            }
+        };
+    }

parts/src/com/xiaomi/settings/refreshrate/RefreshUtils.java
@@ -1,0 +1,141 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.refreshrate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.view.Display;
+
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+public final class RefreshUtils {
+
+    private static final String REFRESH_CONTROL = "refresh_control";
+
+    private static float defaultMaxRate;
+    private static float defaultMinRate;
+    private static final String KEY_PEAK_REFRESH_RATE = "peak_refresh_rate";
+    private static final String KEY_MIN_REFRESH_RATE = "min_refresh_rate";
+    private Context mContext;
+    protected static boolean isAppInList = false;
+
+    protected static final int STATE_DEFAULT = 0;
+    protected static final int STATE_STANDARD = 1;
+    protected static final int STATE_EXTREME = 2;
+
+    private static final float REFRESH_STATE_DEFAULT = 120f;
+    private static final float REFRESH_STATE_STANDARD = 60f;
+    private static final float REFRESH_STATE_EXTREME = 120f;
+
+    private static final String REFRESH_STANDARD = "refresh.standard=";
+    private static final String REFRESH_EXTREME = "refresh.extreme=";
+
+    private SharedPreferences mSharedPrefs;
+
+    protected RefreshUtils(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        mContext = context;
+    }
+
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, RefreshService.class),
+                UserHandle.CURRENT);
+    }
+
+    private void writeValue(String profiles) {
+        mSharedPrefs.edit().putString(REFRESH_CONTROL, profiles).apply();
+    }
+
+   protected void getOldRate(){
+        defaultMaxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, REFRESH_STATE_DEFAULT);
+        defaultMinRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, REFRESH_STATE_DEFAULT);
+    }
+
+
+    private String getValue() {
+        String value = mSharedPrefs.getString(REFRESH_CONTROL, null);
+
+        if (value == null || value.isEmpty()) {
+            value = REFRESH_STANDARD + ":" + REFRESH_EXTREME;
+            writeValue(value);
+        }
+        return value;
+    }
+
+    protected void writePackage(String packageName, int mode) {
+        String value = getValue();
+        value = value.replace(packageName + ",", "");
+        String[] modes = value.split(":");
+        String finalString;
+
+        switch (mode) {
+            case STATE_STANDARD:
+                modes[0] = modes[0] + packageName + ",";
+                break;
+            case STATE_EXTREME:
+                modes[1] = modes[1] + packageName + ",";
+                break;
+        }
+
+        finalString = modes[0] + ":" + modes[1];
+
+        writeValue(finalString);
+    }
+
+    protected int getStateForPackage(String packageName) {
+        String value = getValue();
+        String[] modes = value.split(":");
+        int state = STATE_DEFAULT;
+        if (modes[0].contains(packageName + ",")) {
+            state = STATE_STANDARD;
+        } else if (modes[1].contains(packageName + ",")) {
+            state = STATE_EXTREME;
+        }
+        return state;
+    }
+
+    protected void setRefreshRate(String packageName) {
+        String value = getValue();
+        String modes[];
+        float maxrate = defaultMaxRate;
+        float minrate = defaultMinRate;
+        isAppInList = false;
+
+            if (value != null) {
+            modes = value.split(":");
+
+            if (modes[0].contains(packageName + ",")) {
+                maxrate = REFRESH_STATE_STANDARD;
+                if ( minrate > maxrate){
+                minrate = maxrate;
+                }
+		isAppInList = true;
+           } else if (modes[1].contains(packageName + ",")) {
+                maxrate = REFRESH_STATE_EXTREME;
+                if ( minrate > maxrate){
+                minrate = maxrate;
+                }
+		isAppInList = true;
+           }
+          }
+	Settings.System.putFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, minrate);
+        Settings.System.putFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, maxrate);
+    }
+}