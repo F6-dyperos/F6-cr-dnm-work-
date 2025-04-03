647fb5814fdfafe39b8b79c1cdce751231cade90
Goku <18100664+Vikas-V2@users.noreply.github.com>
2025-04-03 20:01:25 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+rm -rf hardware/qcom-caf/sm8650/audio/agm && git clone  git@github.com:pineapple-platform/android_vendor_qcom_opensource_agm.git  hardware/qcom-caf/sm8650/audio/agm
+rm -rf hardware/qcom-caf/sm8650/audio/pal   &&  git clone  git@github.com:pineapple-platform/android_vendor_qcom_opensource_arpal-lx.git  hardware/qcom-caf/sm8650/audio/pal
+rm -rf hardware/qcom-caf/sm8650/audio/primary-hal && git clone   git@github.com:pineapple-platform/android_hardware_qcom_audio-ar.git  hardware/qcom-caf/sm8650/audio/primary-hal
+rm -rf hardware/qcom-caf/sm8650/data-ipa-cfg-mgr && git clone git@github.com:pineapple-platform/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git  hardware/qcom-caf/sm8650/data-ipa-cfg-mgr
+rm -rf hardware/qcom-caf/sm8650/dataipa && git clone  git@github.com:pineapple-platform/android_vendor_qcom_opensource_dataipa.git hardware/qcom-caf/sm8650/dataipa
+rm -rf hardware/qcom-caf/sm8650/display && git clone  git@github.com:pineapple-platform/android_hardware_qcom_display.git  hardware/qcom-caf/sm8650/display
+rm -rf hardware/qcom-caf/sm8650/media && git clone   git@github.com:PixelOS-Fifteen/hardware_qcom_media.git hardware/qcom-caf/sm8650/media
+rm -rf device/qcom/sepolicy_vndr/sm8650 && git clone git@github.com:pineapple-platform/android_device_qcom_sepolicy_vndr.git  device/qcom/sepolicy_vndr/sm8650
+
+rm -rf hardware/qcom-caf/common && git clone  git@github.com:pineapple-platform/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
+#rm -rf vendor/qcom/opensource/vibrator && git clone git@github.com:PixelOS-Fifteen/vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator/
+#rm -rf  vendor/qcom/opensource/usb git clone -b 15 git@github.com:xiaomi-peridot/vendor_qcom_opensource_usb.git vendor/qcom/opensource/usb
